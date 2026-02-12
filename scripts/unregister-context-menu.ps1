3c56f9925becb73c3055b4ca69d22b0313d68bc1
Nick <lethaldmg@gmail.com>
2026-02-12 16:37:46 +0000

    Fix Windows 11 context-menu registration reliability and diagnostics

---
+++ scripts/unregister-context-menu.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param()
+
+$keys = @(
+  'HKCU:\Software\Classes\*\shell\SpecialCopyBase64',
+  'HKCU:\Software\Classes\AllFilesystemObjects\shell\SpecialCopyBase64',
+  'HKCU:\Software\Classes\Directory\shell\SpecialCopyBase64',
+  'HKCU:\Software\Classes\Directory\Background\shell\SpecialPasteFromClipboard',
+  'HKCU:\Software\Classes\DesktopBackground\Shell\SpecialPasteFromClipboard',
+  'HKCU:\Software\Classes\Directory\Background\shell\SpecialPasteAssemble',
+  'HKCU:\Software\Classes\DesktopBackground\Shell\SpecialPasteAssemble'
+)
+
+foreach ($key in $keys) {
+  if (Test-Path -LiteralPath $key) {
+    Remove-Item -LiteralPath $key -Recurse -Force
+    Write-Host "Removed: $key"
+  }
+}
+
+Write-Host 'Special Paste context menu entries removed.'
